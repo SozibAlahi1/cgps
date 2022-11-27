c5170ae96bfe3a95d730b637575ceab9b92c0d4a
SozibAlahi1 <105582872+SozibAlahi1@users.noreply.github.com>
2022-11-27 23:00:10 +0600

    new update

--- src/component/about.js
+++ src/component/about.js
@@ -1,5 +1,6 @@
 import React from 'react'
-import Header from './Header'
+import Header from './Header';
+import Footer from './Footer';
 function about() {
   return (
     <>
@@ -8,17 +9,38 @@ function about() {
         <div className='container'>
           <div className='row'>
           <div className='col-md-5'>
-          <img src='play.jpeg' className='img-fluid schoolimage' alt='playground' />
+          <img src='play.jpeg' className='img-fluid schoolimage mb-4' alt='playground' />
             </div>
             <div className='col-md-7'>
-                <h1 className='font font-weight-bold'>শিক্ষার্থীদের জন্য মনোরম পরিবেশ</h1>
+                <h1 className='font font-weight-bold atitle'>শিক্ষার্থীদের জন্য মনোরম পরিবেশ</h1>
+                <h4 className='font font-weight-bold ades'>চকছাতারী প্রাইমারী স্কুলের আছে মনোরম পরিবেশ যা প্রত্যেকে শিক্ষার্থীর পড়াশোনা উপর মনোযোগ বাড়ায়। আমাদের স্কুলে নেই কোন কোলাহল নেই কোন অপরিচ্ছন্ন যায়গা । আছে  চমৎকার প্রকৃতি যা পরিবেশের মান কে অক্ষুন্ন রাখে। অসম্ভব সুন্দর মনোরম পরিবেশের জায়গা আমাদের স্কুল ।চারিদিকে সবুজ  বাগান, সুন্দর মনোরম বাতাস যা মন ও দেহ দুটোকেই সুস্থ করতে যথেষ্ট</h4>
             </div>
           </div>
-        
-        
+          <br></br><br></br>
+          <div className='row'>
+          <div className='col-md-7'>
+          <h1 className='font font-weight-bold atitle'>যোগ্যতা সম্পন্ন শিক্ষকমন্ডলী</h1>
+                <h4 className='font font-weight-bold ades'>চকছাতারী প্রাইমারী স্কুলের আছে যোগ্যতাসম্পন্ন শিক্ষকমন্ডলী । একটি ক্লাস পরিচালনা করার জন্য প্রয়োজন পারদর্শী শিক্ষকমন্ডলী। যা চকছাতারী প্রাইমারী স্কুলে বিদ্যমান। স্কুলের সকল শিক্ষকগন অনেক ভালো এবং বন্ধুসূলভ আচরনের মাধ্যমে শিক্ষার্থীদের ক্লাস পরিচালনা করে থাকেন।</h4>
+          
+            </div>
+            <div className='col-md-5'>
+            <img src='skillteacher.jpg' className='img-fluid schoolimage' alt='playground' />
+            </div>
+          </div>
+          <br></br><br></br>
+          <div className='row'>
+          <div className='col-md-5'>
+          <img src='technical support.png' className='img-fluid schoolimage mb-4' alt='playground' />
+            </div>
+            <div className='col-md-7'>
+                <h1 className='font font-weight-bold atitle'>সেরা সাপোর্ট </h1>
+                <h4 className='font font-weight-bold ades'>চকছাতারী প্রাইমারী স্কুলের আছে মনোরম পরিবেশ যা প্রত্যেকে শিক্ষার্থীর পড়াশোনা উপর মনোযোগ বাড়ায়। আমাদের স্কুলে নেই কোন কোলাহল নেই কোন অপরিচ্ছন্ন যায়গা । আছে  চমৎকার প্রকৃতি যা পরিবেশের মান কে অক্ষুন্ন রাখে। অসম্ভব সুন্দর মনোরম পরিবেশের জায়গা আমাদের স্কুল ।চারিদিকে সবুজ  বাগান, সুন্দর মনোরম বাতাস যা মন ও দেহ দুটোকেই সুস্থ করতে যথেষ্ট</h4>
+            </div>
+          </div>
+          <br></br><br></br>
         </div>
       
-
+        <Footer />
 
 
     </>
