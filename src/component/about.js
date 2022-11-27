045ee41a41eb5e9f55db6c1a043b5182774fce69
SozibAlahi1 <105582872+SozibAlahi1@users.noreply.github.com>
2022-11-27 14:05:49 +0600

    Update about.js

--- src/component/about.js
+++ src/component/about.js
@@ -7,8 +7,11 @@ function about() {
       <h1 className='text-center shadow p-3 mb-5 bg-body rounded font'> আমাদের সম্পর্কে জানুন</h1>
         <div className='container'>
           <div className='row'>
-          <div className='col-md-6'>
+          <div className='col-md-5'>
           <img src='play.jpeg' className='img-fluid schoolimage' alt='playground' />
+            </div>
+            <div className='col-md-7'>
+                <h1 className='font font-weight-bold'>শিক্ষার্থীদের জন্য মনোরম পরিবেশ</h1>
             </div>
           </div>
         
